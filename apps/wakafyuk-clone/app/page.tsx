edfead8a0f30f3954d5b10015afa900fb1a450d2
nadella987 <102640477+nadella987@users.noreply.github.com>
2023-10-02 14:34:08 +0700

    feat: konfigurasi firebase

--- apps/wakafyuk-clone/app/page.tsx
+++ apps/wakafyuk-clone/app/page.tsx
@@ -1,6 +1,34 @@
+'use client';
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc } from 'firebase/firestore/lite';
+
 import styles from './page.module.css';
+import { useEffect } from 'react';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCl_JvzTSk-3Ogdw91bcfneGPbJNZN3Ndo',
+  authDomain: 'wakaf-e5fe3.firebaseapp.com',
+  projectId: 'wakaf-e5fe3',
+  storageBucket: 'wakaf-e5fe3.appspot.com',
+  messagingSenderId: '104381034621',
+  appId: '1:104381034621:web:bf00af6093820d24be0114',
+  measurementId: 'G-21M21SC4F8',
+};
 
 export default async function Index() {
+  function testConnection() {
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const docRef = doc(db, 'tes', 'b2M5mhawcb4igkUaYvU7');
+    getDoc(docRef).then((snap) => {
+      alert(snap.get('message'));
+    });
+  }
+
+  useEffect(() => {
+    testConnection();
+  }, []);
   /*
    * Replace the elements below with your own.
    *
